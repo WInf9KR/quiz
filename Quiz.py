76dac6bf05b16afe282b6cd576b43e0d453b6703
WInf9KR <WInf9KR@users.noreply.github.com>
2015-03-27 19:45:22 +0100

    Update and rename Quiz5.0.py to Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -5,6 +5,7 @@ Quiz = json.loads(in_file.read())
 in_file.close()
 
 SL=[]
+AnzSp = 0
       
 def players():
     AnzSp=int(input("Wie viele Spieler spielen mit?""\t"))
@@ -27,12 +28,14 @@ def correct_answer(L,r,RAntwort):
     if RAntwort==L[r][2] :
         correct = True
     else:
-        correct =False
+        correct = False
     return correct
 
 def result_answers(y,r,L,SL):
     print("Die richtige Antwort war ",L[r][2],"\n") 
-    for i in range(0,y):
+    # Folgendes wird auch ausgeführt, wenn Quiz zuende ist, wird
+    # also doppelt ausgegeben. Bitte noch ändern!
+    for i in range(0,len(SL)):
         print(SL[i][0], "hat momentan",SL[i][1],"Punkt(e).""\n")
 
 def give_player_key():
@@ -43,48 +46,56 @@ def give_player_key():
         key = "Taste von Spieler "+ str(i)+":"
         SpKey = input(key)
         SL.append([SpName,0,SpKey])
-    return AnzSp
 
 def get_player_by_key(pressedKey):
     for i in range(0,len(SL)):
         if SL[i][2] == pressedKey:
-            foundPlayer = SL[i][2]
-            return foundPlayer
+            #foundPlayer = SL[i]
+            return i
 
 print("Verfügbare Spielarten:")
 print("1 - Jeder Spieler gibt eine Antwort ab.")
 print("2 - Jeder Spieler bekommt eine Taste zugewiesen, drückt er diese,\nist er der einzige der die Frage beantworten darf.")
 Spielart = input("Welche möchtest du nutzen?")
 
-if Spielart == 1:
+if int(Spielart) == 1:
     y=players()        
     f=len(Quiz)
     for o in range (0,f):
         print_question(Quiz,o)
         print_answers(Quiz, o)
+        richtig = False
         for u in range(0,y):
             print(SL[u][0],"ist an der Reihe") 
             x=input("Antwort:")
-        
             if correct_answer(Quiz,o,x):
                 SL[u][1]=SL[u][1]+1
-        if o != f-1:
+                richtig = True
+        if richtig == False:
             result_answers(y,o,Quiz,SL)
     for i in range(0,y):
         print(SL[i][0],"hat",SL[i][1],"Punkt(e) von möglichen",f,"Punkten erreicht")
-
-if Spielart == 2:        
-    y=give_player_key()        
+else:        
+    y=give_player_key()
     f=len(Quiz)
     for o in range (0,f):
         print_question(Quiz,o)
         print_answers(Quiz, o)
         PressedKey = input("Wenn du die Antwort weißt, drücke deine Taste!")
-            print(SL[u][0],"ist an der Reihe")
-            x=input("Antwort:")
-            if correct_answer(Quiz,o,x):
-                SL[u][1]=SL[u][1]+1
-        if o != f-1:
+        getPlayer = get_player_by_key(PressedKey)
+        # Hier werden mindestens ZWEI Spieler benötigt!
+        print(SL[getPlayer][0],"ist an der Reihe.")
+        x = input("Welche Antwort ist richtig?")
+        richtig = False
+        if correct_answer(Quiz,o,x):
+                SL[getPlayer][1]=SL[getPlayer][1]+1
+                richtig = True
+                print("Das war richtig!")
+                # Folgendes wird auch ausgeführt, wenn Quiz zuende ist, wird
+                # also doppelt ausgegeben. Bitte noch ändern!
+                for i in range(0,len(SL)):
+                    print(SL[i][0], "hat momentan",SL[i][1],"Punkt(e).""\n")
+        if richtig == False:
             result_answers(y,o,Quiz,SL)
-    for i in range(0,y):
-        print(SL[i][0],"hat",SL[i][1],"Punkt(e) von möglichen",f,"Punkten erreicht")
+    for i in range(0,len(SL)):
+        print(SL[i][0],"hat",SL[i][1],"Punkt(e) von",f," möglichen Punkten erreicht.")
