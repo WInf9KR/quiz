0cd7cccf30fb41792b9111d60ff345e6cfb35ed9
WInf9KR <WInf9KR@users.noreply.github.com>
2015-03-30 16:52:17 +0200

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,59 +1,26 @@
 import json
+from PlayerListProcessors import *
+from PrintTexts import *
+from AnswerProcessors import *
+from getPlayerByKey import *
 
+# Fragen importieren
 in_file= open ("Fragen.txt","rt")
 Quiz = json.loads(in_file.read())
 in_file.close()
 
+# Spielerliste importieren
 SL=[]
-      
-def players():
-    AnzSp=int(input("Wie viele Spieler spielen mit?""\t"))
-    for i in range(1,AnzSp+1):
-        spieler = "Name von Spieler "+ str(i)+":"
-        SpName=input(spieler)
-        SL.append([SpName,0])
-    return AnzSp
+in_file = open ("playerList.txt","rt")
+SL = json.loads(in_file.read())
+in_file.close()
 
-def print_question(L,r):
-    print(L[r][0])
+def refreshPlayerList():
+    in_file = open ("playerList.txt","rt")
+    SL = json.loads(in_file.read())
+    in_file.close()
 
-def print_answers(L,r):
-    länge = len(L[r][1])
-    for i in range(0,länge):
-        print(L[r][1][i])
-
-def correct_answer(L,r,RAntwort):
-    Antworten=L[r][2]
-    if RAntwort==L[r][2] :
-        correct = True
-    else:
-        correct =False
-    return correct
-
-def result_answers(r,L,SL):
-    print("Die richtige Antwort war ",L[r][2],"\n") 
-    # Folgendes wird auch ausgeführt, wenn Quiz zuende ist, wird
-    # also doppelt ausgegeben. Bitte noch ändern!
-
-def give_player_key():
-    AnzSp=int(input("Wie viele Spieler spielen mit?""\t"))
-    for i in range(1,AnzSp+1):
-        spieler = "Name von Spieler "+ str(i)+":"
-        SpName=input(spieler)
-        key = "Taste von Spieler "+ str(i)+":"
-        SpKey = input(key)
-        SL.append([SpName,0,SpKey])
-
-def get_player_by_key(pressedKey):
-    for i in range(0,len(SL)):
-        if SL[i][2] == pressedKey:
-            #foundPlayer = SL[i]
-            return i
-
-def print_points():
-    for i in range(0,len(SL)):
-        print(SL[i][0], "hat momentan",SL[i][1],"Punkt(e).""\n")
-
+# Gewünschte Spielvariante abfragen
 print("Verfügbare Spielvarianten:")
 print("1 - Jeder Spieler gibt eine Antwort ab.")
 print("2 - Jeder Spieler bekommt eine Taste zugewiesen, drückt er diese,\nist er der einzige der die Frage beantworten darf.")
@@ -66,17 +33,19 @@ if int(Spielart) == 1:
       for o in range (0,f):
           print_question(Quiz,o)
           print_answers(Quiz, o)
-          richtig = False
+          richtig = True
           for u in range(0,y):
               print(SL[u][0],"ist an der Reihe") 
               x=input("Antwort:")
               if correct_answer(Quiz,o,x):
                   SL[u][1]=SL[u][1]+1
                   richtig = True
-           result_answers(o,Quiz,SL)
-           print_points() 
+          if richtig == True:
+              result_answers(o,Quiz,SL)
+      refreshPlayerList()
       for i in range(0,y):
           print(SL[i][0],"hat",SL[i][1],"Punkt(e) von",f,"möglichen Punkten erreicht")
+      emptyPlayerList()
 else:        
     # Spielvariante 2
     give_player_key()
@@ -85,19 +54,13 @@ else:
         print_question(Quiz,o)
         print_answers(Quiz, o)
         PressedKey = input("Wenn du die Antwort weißt, drücke deine Taste!")
-        getPlayer = get_player_by_key(PressedKey)
+        getPlayer = getPlayerByKey(PressedKey)
+        # Hier werden mindestens ZWEI Spieler benötigt!
         print(SL[getPlayer][0],"ist an der Reihe.")
         x = input("Welche Antwort ist richtig?")
-        richtig = False
-        if correct_answer(Quiz,o,x):
-                SL[getPlayer][1]=SL[getPlayer][1]+1
-                richtig = True
-                print("Das war richtig!")
-                # Folgendes wird auch ausgeführt, wenn Quiz zuende ist, wird
-                # also doppelt ausgegeben. Bitte noch ändern!
-                print_points()
-        if richtig == False:
-            result_answers(o,Quiz,SL)
-            print_points()
+        punkte_rechnen(Quiz,o,x, PressedKey)
+        result_answers(o,Quiz,SL)
+    refreshPlayerList()
     for i in range(0,len(SL)):
         print(SL[i][0],"hat",SL[i][1],"Punkt(e) von",f," möglichen Punkten erreicht.")
+    emptyPlayerList()
