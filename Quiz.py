aebeb7ba26f75419c6e0a7ffa6046120abfbd4bf
WInf9KR <WInf9KR@users.noreply.github.com>
2015-04-01 16:03:13 +0200

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -33,15 +33,12 @@ if int(Spielart) == 1:
       for o in range (0,f):
           print_question(Quiz,o)
           print_answers(Quiz, o)
-          richtig = True
           for u in range(0,y):
               print(SL[u][0],"ist an der Reihe") 
               x=input("Antwort:")
               if correct_answer(Quiz,o,x):
                   SL[u][1]=SL[u][1]+1
-                  richtig = True
-          if richtig == True:
-              result_answers(o,Quiz,SL)
+          result_answers(o,Quiz,SL)
       in_file = open ("playerList.txt","rt")
       SL = json.loads(in_file.read())
       in_file.close()
@@ -59,7 +56,6 @@ else:
         print_answers(Quiz, o)
         PressedKey = input("Wenn du die Antwort weißt, drücke deine Taste!")
         getPlayer = getPlayerByKey(PressedKey)
-        # Hier werden mindestens ZWEI Spieler benötigt!
         print(SL[getPlayer][0],"ist an der Reihe.")
         x = input("Welche Antwort ist richtig?")
         punkte_rechnen(Quiz,o,x, PressedKey)
